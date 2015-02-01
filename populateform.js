fdc782fb944449ce724fba613011d6a6c5ecd4cf
fitnr <fitnr@fakeisthenewreal>
2015-02-01 13:06:03 -0500

    simplify license

--- populateform.js
+++ populateform.js
@@ -2,10 +2,7 @@
     Populateform - Populate a form with data from a query string
     http://github.com/fitnr/populateform
     Copyright (C) 2014-2015 Neil Freeman
-
-    This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+    License GPL v3
 */
 ! function(name, definition) {
 
